bb46edd5915bdc22d3780c79683377477bcf9870
alexbckr <amb3yn@virginia.edu>
2022-07-20 13:02:00 -0700

    simplify styling, removed a button

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -1,43 +1,46 @@
-import styles from '../styles/CustomNavbar.module.css'
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { userInfo } from 'os';
-import { Router } from 'next/router';
+import styles from "../styles/CustomNavbar.module.css"
+import { signIn, signOut, useSession } from "next-auth/react"
+import { userInfo } from "os"
+import { Router } from "next/router"
 
 export default function CustomNavbar(props) {
-    const { data: session, status } = useSession();
+   const { data: session, status } = useSession()
 
-    return (
-        <div className={styles.navbar}>
-            <a className={styles.brand_no_directory} href="/">VRPay</a>
+   return (
+      <div className={styles.navbar}>
+         <a className={styles.brand} href="/">
+            VRPay
+         </a>
 
-            {session?.is_verified &&
-                <div align='left' className={styles.verifiedSessionButtons}>
-                    <a href={`/u/${session.uid}`}>
-                        My Dues
-                    </a>
-                    {session.role == "admin" &&
-                        <a href="/dashboard" className={styles.adminOptions}>
-                            Admin
-                        </a>
-                    }
-                </div>
-            }
+         <div className={styles.verifiedSessionButtons}>
+            {session?.role == "admin" && (
+               <a href="/dashboard" className={styles.adminButton}>
+                  Admin
+               </a>
+            )}
+         </div>
 
-            <div className={styles.navbarRight}>
-                {session?.user &&
-                    <div>
-                        <a className={styles.navButton} href={`/api/auth/signout`} onClick={(e) => {
-                            e.preventDefault()
-                            signOut({
-                                callbackUrl: `${window.location.origin}`
-                            })
-                        }}>Sign Out</a>
-                    </div>
-                }
-            </div>
-            <div className={styles.navbarRight}>
-                {!session?.user && <p className={styles.navButton} onClick={() => signIn("google")}>Sign in</p>}
-            </div>
-        </div>
-    )
+         <div className={styles.navbarRight}>
+            {session?.user && (
+               <a
+                  className={styles.navButton} 
+                  href={`/api/auth/signout`}
+                  onClick={(e) => {
+                     e.preventDefault()
+                     signOut({
+                        callbackUrl: `${window.location.origin}`,
+                     })
+                  }}
+               >
+                  Sign Out
+               </a>
+            )}
+            {!session?.user && (
+               <p className={styles.navButton} onClick={() => signIn("google")}>
+                  Sign in
+               </p>
+            )}
+         </div>
+      </div>
+   )
 }
