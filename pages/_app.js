2848b1dcaaa27fd0d0e87c40731e66d82ba6b986
alexbckr <amb3yn@virginia.edu>
2022-08-28 17:48:37 -0400

    adding styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,23 @@
-import '../styles/globals.css'
-import { SessionProvider } from 'next-auth/react'
-import Layout from '../components/Layout'
-import Head from 'next/head'
+import "../styles/globals.css"
+import { SessionProvider } from "next-auth/react"
+import Layout from "../components/Layout"
+import Head from "next/head"
+// import { GeistProvider, CssBaseline } from "@geist-ui/core"
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session} refetchInterval={0}>
-      <Head>
-          <title>VRPay</title>
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SessionProvider>
-  )
+   return (
+      <SessionProvider session={pageProps.session} refetchInterval={0}>
+         <Head>
+            <title>VRPay</title>
+         </Head>
+         {/* <GeistProvider>
+            <CssBaseline /> */}
+            <Layout>
+               <Component {...pageProps} />
+            </Layout>
+         {/* </GeistProvider> */}
+      </SessionProvider>
+   )
 }
 
 export default MyApp
