e61fcc0e7fcc9db4810562ce99884a5541e06d90
alexbckr <amb3yn@virginia.edu>
2022-09-14 11:23:47 -0400

    add delete button for transactions

--- components/TransactionDetailsContent.js
+++ components/TransactionDetailsContent.js
@@ -5,39 +5,48 @@ import { formatMoney, getDateFormatting } from "../utils"
 
 export default function NewTransactionContent(props) {
    return (
-      <div className={styles.container} onClick={() => console.log("props", props)}>
+      <div
+         className={styles.container}
+         onClick={() => console.log("props", props)}
+      >
          <div className={styles.container}>
-         <Modal.Title style={{ textTransform: "None" }}>
-            Viewing Transaction {props.name ? "for " + props.name : ""}
-         </Modal.Title>
-         <div className={styles.inputSection}>
-            <div className={styles.inputGroup}>
-               <b className={styles.label}>Amount</b>
-               <p>{formatMoney.format(props?.transaction?.amount)}</p>
+            <Modal.Title style={{ textTransform: "None" }}>
+               Viewing Transaction {props.name ? "for " + props.name : ""}
+            </Modal.Title>
+            <div className={styles.inputSection}>
+               <div className={styles.inputGroup}>
+                  <b className={styles.label}>Amount</b>
+                  <p>{formatMoney.format(props?.transaction?.amount)}</p>
+               </div>
+               <div className={styles.inputGroup}>
+                  <b className={styles.label}>Type</b>
+                  <p>{props?.transaction?.type}</p>
+               </div>
+               <div className={styles.inputGroup}>
+                  <b className={styles.label}>Last updated</b>
+                  <p>{getDateFormatting(props?.transaction?.updatedAt)}</p>
+               </div>
             </div>
-            <div className={styles.inputGroup}>
-               <b className={styles.label}>Type</b>
-               <p>{props?.transaction?.type}</p>
+            <div className={styles.inputSection}>
+               <div className={styles.inputGroup}>
+                  <b className={styles.label}>Description</b>
+                  <p>{props?.transaction?.description}</p>
+               </div>
             </div>
-            <div className={styles.inputGroup}>
-               <b className={styles.label}>Last updated</b>
-               <p>{getDateFormatting(props?.transaction?.updatedAt)}</p>
-            </div>
-         </div>
-         <div className={styles.inputSection}>
-            <div className={styles.inputGroup}>
-               <b className={styles.label}>Description</b>
-               <p>{props?.transaction?.description}</p>
-            </div>
-         </div>
-         {props.transaction.status === "pending" && 
-            <div className={styles.actions}>
-               <Button auto type="abort">Edit</Button>
-               <Button auto type="error">Delete</Button>
-               <Button auto type="success">Approve</Button>
-            </div>
-         }
 
+            <div className={styles.actions}>
+               <Button auto type="abort">
+                  Edit
+               </Button>
+               <Button auto type="error">
+                  Delete
+               </Button>
+               {props.transaction.status === "pending" && (
+                  <Button auto type="success">
+                     Approve
+                  </Button>
+               )}
+            </div>
          </div>
       </div>
    )
