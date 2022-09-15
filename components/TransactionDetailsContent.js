cd6716d587c37534f174fbacd4256fc15954c707
alexbckr <amb3yn@virginia.edu>
2022-09-14 22:22:25 -0400

    likely first steps to editing transactions

--- components/TransactionDetailsContent.js
+++ components/TransactionDetailsContent.js
@@ -1,10 +1,18 @@
 import styles from "../styles/Overlays.module.css"
 import { useEffect, useRef, useState } from "react"
 import { Input, Button, Modal, Select } from "@geist-ui/core"
-import { formatMoney, getDateFormatting, approveTransaction, isTransactionValid, deleteTransaction } from "../utils"
+import {
+   formatMoney,
+   getDateFormatting,
+   approveTransaction,
+   isTransactionValid,
+   deleteTransaction,
+} from "../utils"
 import Router from "next/router"
 
 export default function NewTransactionContent(props) {
+   const [isEditing, setIsEditing] = useState(false)
+
    return (
       <div
          className={styles.container}
@@ -16,8 +24,22 @@ export default function NewTransactionContent(props) {
             </Modal.Title>
             <div className={styles.inputSection}>
                <div className={styles.inputGroup}>
-                  <b className={styles.label}>Amount</b>
-                  <p>{formatMoney.format(props?.transaction?.amount)}</p>
+                  {isEditing ? (
+                     <Input
+                        className={styles.formInput}
+                        onChange={(e) => setAmount(e.target.value)}
+                        initialValue={props?.transaction?.amount}
+                        width={"100%"}
+                     >
+                        <b>Amount</b>
+                     </Input>
+                  ) : (
+                     <div>
+                        <b className={styles.label}>Amount</b>
+
+                        <p>{formatMoney.format(props?.transaction?.amount)}</p>
+                     </div>
+                  )}
                </div>
                <div className={styles.inputGroup}>
                   <b className={styles.label}>Type</b>
@@ -36,13 +58,33 @@ export default function NewTransactionContent(props) {
             </div>
 
             <div className={styles.actions}>
-               <Button auto type="abort">
-                  Edit
+               <Button
+                  auto
+                  type="abort"
+                  onClick={() => {
+                     if (isEditing) {
+                        setIsEditing(false)
+                        //handle cancel
+                     } else {
+                        setIsEditing(true)
+                     }
+                  }}
+               >
+                  {isEditing ? "Cancel" : "Edit"}
                </Button>
-               <Button auto type="error" onClick={() => {
-                  deleteTransaction(props?.transaction?.id)
-               }}>
-                  Delete
+               <Button
+                  auto
+                  type={isEditing ? "success" : "error"}
+                  onClick={() => {
+                     if (isEditing) {
+                        //handle save
+                        setIsEditing(false)
+                     } else {
+                        deleteTransaction(props?.transaction?.id)
+                     }
+                  }}
+               >
+                  {isEditing ? "Save" : "Delete"}
                </Button>
                {props?.transaction?.status === "pending" && (
                   <Button
